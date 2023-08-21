f219f9fc498f55177ae3085a332ace521e824716
Saint_movicx <victoride013@gmail.com>
2023-08-21 16:01:43 +0100

    first commit

---
+++ Items.js
@@ -0,0 +1,385 @@
+const items = [
+  {
+    id: 1,
+    studentsId: 'PO16 7GZ',
+  },
+  {
+    id: 2,
+    studentsId: 'KY11 8ST',
+  },
+  {
+    id: 3,
+    studentsId: 'BA3 4YF',
+  },
+  {
+    id: 4,
+    studentsId: 'DY13 0NW',
+  },
+  {
+    id: 5,
+    studentsId: 'L20 7YW',
+  },
+  {
+    id: 6,
+    studentsId: 'WS13 8UR',
+  },
+  {
+    id: 7,
+    studentsId: 'SR43 2SB',
+  },
+  {
+    id: 8,
+    studentsId: 'SA99 1BT ',
+  },
+  {
+    id: 9,
+    studentsId: 'UB7 0LB ',
+  },
+  {
+    id: 11,
+    studentsId: 'SW1A 2AA ',
+  },
+  {
+    id: 12,
+    studentsId: 'PR3 1XB ',
+  },
+  {
+    id: 13,
+    studentsId: 'BT2 8HQ ',
+  },
+  {
+    id: 14,
+    studentsId: 'G51 1DA',
+  },
+  {
+    id: 15,
+    studentsId: 'SW1W 0NY',
+  },
+  {
+    id: 16,
+    studentsId: 'B15 2TE ',
+  },
+  {
+    id: 17,
+    studentsId: 'M9 5SD ',
+  },
+  {
+    id: 18,
+    studentsId: 'EH3 1GE ',
+  },
+  {
+    id: 19,
+    studentsId: 'PR25 9FR ',
+  },
+  {
+    id: 20,
+    studentsId: 'SA99 1YW ',
+  },
+  {
+    id: 21,
+    studentsId: 'SW1A 0AA ',
+  },
+  {
+    id: 22,
+    studentsId: 'AL10 9WN ',
+  },
+  {
+    id: 23,
+    studentsId: 'W1A 6US ',
+  },
+  {
+    id: 24,
+    studentsId: 'CV3 9SD ',
+  },
+  {
+    id: 25,
+    studentsId: 'SR5 9QY ',
+  },
+  {
+    id: 26,
+    studentsId: 'IG11 7RN ',
+  },
+  {
+    id: 27,
+    studentsId: 'N1 7GU ',
+  },
+  {
+    id: 28,
+    studentsId: 'NE12 8NW ',
+  },
+  {
+    id: 29,
+    studentsId: 'NN18 8UA ',
+  },
+  {
+    id: 30,
+    studentsId: 'DL98 1AT ',
+  },
+  {
+    id: 31,
+    studentsId: 'BT1 9WY ',
+  },
+  {
+    id: 32,
+    studentsId: 'EC1A 1AA ',
+  },
+  {
+    id: 33,
+    studentsId: 'DA15 0BQ ',
+  },
+  {
+    id: 34,
+    studentsId: 'M5 4WT ',
+  },
+
+  {
+    id: 35,
+    studentsId: 'PE32 1TE ',
+  },
+  {
+    id: 36,
+    studentsId: 'SO14 0AG ',
+  },
+  {
+    id: 37,
+    studentsId: 'HD8 1DY ',
+  },
+  {
+    id: 38,
+    studentsId: 'DL1 9AG ',
+  },
+  {
+    id: 39,
+    studentsId: 'WV98 2AD ',
+  },
+  {
+    id: 40,
+    studentsId: 'CV4 7ES ',
+  },
+  {
+    id: 41,
+    studentsId: 'N15 6RS ',
+  },
+  {
+    id: 42,
+    studentsId: 'BT1 9PT ',
+  },
+  {
+    id: 43,
+    studentsId: 'CV21 9FU ',
+  },
+  {
+    id: 44,
+    studentsId: 'BS9 0DP ',
+  },
+  {
+    id: 45,
+    studentsId: 'BN21 9AT ',
+  },
+  {
+    id: 1,
+    studentsId: 'E1 6AN ',
+  },
+  {
+    id: 46,
+    studentsId: 'WV14 4BZ ',
+  },
+  {
+    id: 47,
+    studentsId: 'M1 1AG ',
+  },
+  {
+    id: 48,
+    studentsId: 'E1 6JJ ',
+  },
+  {
+    id: 49,
+    studentsId: 'PR3 1NJ ',
+  },
+  {
+    id: 50,
+    studentsId: 'SY5 8NF ',
+  },
+  {
+    id: 51,
+    studentsId: 'WA7 9DY ',
+  },
+  {
+    id: 52,
+    studentsId: 'BR1 3BX ',
+  },
+  {
+    id: 53,
+    studentsId: 'CV4 7AL ',
+  },
+  {
+    id: 54,
+    studentsId: 'NG20 8RY ',
+  },
+  {
+    id: 55,
+    studentsId: 'NE38 9EX ',
+  },
+  {
+    id: 56,
+    studentsId: 'M4 6GA ',
+  },
+  {
+    id: 57,
+    studentsId: 'FY8 5DX ',
+  },
+  {
+    id: 58,
+    studentsId: 'BH16 6FA ',
+  },
+  {
+    id: 59,
+    studentsId: 'SK14 4TU ',
+  },
+  {
+    id: 60,
+    studentsId: 'BN1 6WH ',
+  },
+  {
+    id: 61,
+    studentsId: 'WC1X 0HD ',
+  },
+  {
+    id: 62,
+    studentsId: 'DE1 0NQ ',
+  },
+  {
+    id: 63,
+    studentsId: 'NP11 4RB ',
+  },
+  {
+    id: 64,
+    studentsId: 'E17 9RD ',
+  },
+  {
+    id: 65,
+    studentsId: 'WA5 9QG ',
+  },
+  {
+    id: 66,
+    studentsId: 'SN38 1NW ',
+  },
+  {
+    id: 67,
+    studentsId: 'CV34 4LL ',
+  },
+  {
+    id: 68,
+    studentsId: 'SA6 7JL ',
+  },
+
+  {
+    id: 69,
+    studentsId: 'IV11 8YS ',
+  },
+  {
+    id: 70,
+    studentsId: 'SA99 1AB ',
+  },
+  {
+    id: 71,
+    studentsId: 'ST4 5BW',
+  },
+  {
+    id: 72,
+    studentsId: 'SA99 1ZW',
+  },
+  {
+    id: 73,
+    studentsId: 'EX2 7JH',
+  },
+  {
+    id: 74,
+    studentsId: 'EH54 0HH',
+  },
+  {
+    id: 75,
+    studentsId: 'LE87 2GA',
+  },
+  {
+    id: 76,
+    studentsId: 'M14 4TY',
+  },
+  {
+    id: 77,
+    studentsId: 'BN3 8EZ',
+  },
+  {
+    id: 78,
+    studentsId: 'SA99 1BN',
+  },
+  {
+    id: 79,
+    studentsId: 'CF10 4GA',
+  },
+  {
+    id: 80,
+    studentsId: 'EC1A 1BB',
+  },
+  {
+    id: 81,
+    studentsId: 'LE4 6JL',
+  },
+  {
+    id: 82,
+    studentsId: 'SA99 1AF',
+  },
+  {
+    id: 83,
+    studentsId: 'S60 1AA',
+  },
+  {
+    id: 84,
+    studentsId: 'BN99 6RB',
+  },
+  {
+    id: 85,
+    studentsId: 'NW8 0EG',
+  },
+  {
+    id: 86,
+    studentsId: 'M21 9NP',
+  },
+  {
+    id: 87,
+    studentsId: 'NE98 1ZZ',
+  },
+  {
+    id: 88,
+    studentsId: 'DE12 6RE',
+  },
+  {
+    id: 89,
+    studentsId: 'N5 1AX',
+  },
+  {
+    id: 90,
+    studentsId: 'BH16 6FH',
+  },
+]
+const itemContainer = document.getElementById('itemContainer');
+
+items.forEach(item => {
+  const itemDiv = document.createElement('div');
+  itemDiv.className = 'item';
+
+  const itemName = document.createElement('h3');
+  
+ 
+  itemName.textContent = item.studentsId;
+
+  
+
+const itemDescription = document.createElement('p');
+  itemDescription.textContent = item.description;
+
+  itemDiv.appendChild(itemName);
+  itemDiv.appendChild(itemDescription);
+
+  itemContainer.appendChild(itemDiv);
+});
---
+++ main.js
@@ -0,0 +1,46 @@
+//all dom here
+const dropdownButton = document.querySelector('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+const dropdownButton2 = document.querySelector('.dropdown-btn2');
+const dropdownContent2 = document.querySelector('.dropdown-content2');
+const dropdownButton3 = document.querySelector('.dropdown-btn3');
+const dropdownContent3 = document.querySelector('.dropdown-content3');
+
+// for the modals
+dropdownButton.addEventListener('click', () => {
+    dropdownContent.classList.toggle('show');
+});
+
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropdown-btn')) {
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+});
+//for the modals
+dropdownButton2.addEventListener('click', () => {
+  dropdownContent2.classList.toggle('show');
+});
+
+window.addEventListener('click', (event) => {
+  if (!event.target.matches('.dropdown-btn2')) {
+      if (dropdownContent2.classList.contains('show')) {
+          dropdownContent2.classList.remove('show');
+      }
+  }
+});
+//for the modals
+dropdownButton3.addEventListener('click', () => {
+  dropdownContent3.classList.toggle('show');
+});
+
+window.addEventListener('click', (event) => {
+  if (!event.target.matches('.dropdown-btn3')) {
+      if (dropdownContent3.classList.contains('show')) {
+          dropdownContent3.classList.remove('show');
+      }
+  }
+});
+
+
